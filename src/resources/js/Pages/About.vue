ac6f58119779d0ee182b28defe16a8eb8fbe93b4
LittleTruck <i839855314i@gmail.com>
2021-07-11 21:34:20 +0800

    circle layout & scale animation

--- src/resources/js/Pages/About.vue
+++ src/resources/js/Pages/About.vue
@@ -1,95 +1,67 @@
 ﻿<template>
     <div class="about">
-        <h2 class="container-fluid">
-            <swiper class="swiper" :options="swiperOption">
-                <swiper-slide>111</swiper-slide>
-                <swiper-slide>222</swiper-slide>
-                <swiper-slide>333</swiper-slide>
-                <swiper-slide>444</swiper-slide>
-                <swiper-slide>555</swiper-slide>
-                <swiper-slide>666</swiper-slide>
-                <swiper-slide>777</swiper-slide>
-                <div class="swiper-scrollbar" slot="scrollbar"></div>
-            </swiper>
-            <div class="row" style="margin: 500px">
-                <div class="col-md-4">111</div>
-                <div class="col-md-4">222</div>
-                <div class="col-md-4">333</div>
-                <div class="col-md-4">444</div>
+        <kinesis-container>
+
+            <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <kinesis-element class="circle-style" :strength="50"><div class="circle-inner"> 1</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="75"><div class="circle-inner"> 2</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="100"><div class="circle-inner"> 3</div></kinesis-element>
+                    </div>
+
+
+                    <div class="row justify-content-center">
+                        <kinesis-element class="circle-style" :strength="50"><div class="circle-inner"> 4</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="75"><div class="circle-inner"> 5</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="100"><div class="circle-inner"> 6</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="125"><div class="circle-inner"> 6</div></kinesis-element>
+                    </div>
+
+                    <div class="row justify-content-center">
+                        <kinesis-element class="circle-style" :strength="50"><div class="circle-inner"> 7</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="75"><div class="circle-inner"> 8</div></kinesis-element>
+                        <kinesis-element class="circle-style" :strength="100"><div class="circle-inner"> 9</div></kinesis-element>
+                    </div>
             </div>
-            <div data-aos-anchor-placement="top-bottom" data-aos="fade-down" class="row">
-                <div class="col-md-4">111</div>
-                <div class="col-md-4">222</div>
-                <div class="col-md-4">333</div>
-                <div class="col-md-4">444</div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">111</div>
-                <div class="col-md-4">222</div>
-                <div class="col-md-4">333</div>
-                <div class="col-md-4">444</div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">111</div>
-                <div class="col-md-4">222</div>
-                <div class="col-md-4">333</div>
-                <div class="col-md-4">444</div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">111</div>
-                <div class="col-md-4">222</div>
-                <div class="col-md-4">333</div>
-                <div class="col-md-4">444</div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">111</div>
-                <div class="col-md-4">222</div>
-                <div class="col-md-4">333</div>
-                <div class="col-md-4">444</div>
-            </div>
-        </h2>
+        </kinesis-container>
     </div>
 </template>
 
 <script>
-    import Vue from 'vue';
-    import VueAwesomeSwiper from  'vue-awesome-swiper'
-    import{swiper, swiperSlide} from 'vue-awesome-swiper'
-    import 'swiper/dist/css/swiper.css'
-    Vue.use(VueAwesomeSwiper);
+    import {KinesisContainer, KinesisElement} from 'vue-kinesis'
 
     export default {
         name: "About",
-        title: 'Free mode / No fixed positions',
         components: {
-            swiper,
-            swiperSlide
-        },
-        data() {
-            return {
-                swiperOption: {
-                    slidesPerView: 4,
-                    spaceBetween: 30,
-                    freeMode: true,
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev'
-                    },
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable: true
-                    },
-                    scrollbar: {
-                        el: '.swiper-scrollbar'
-                    },
-                    mousewheel: true,
-                }
-            }
-        },
+            KinesisContainer,
+            KinesisElement
+        }
     }
 </script>
 
 <style scoped>
-.about{
-}
+
+    .circle-style{
+        height: 250px;
+        width: 250px;
+        padding: 0 !important;
+        margin: 25px;
+        transition: .5s;
+    }
+    .circle-inner{
+        height: 100%;
+        width: 100%;
+        border: solid 1px black;
+        border-radius: 100%;
+        transition: .5s;
+        transform-origin: center center;
+    }
+    .circle-inner:hover{
+        transform: scale(1.2);
+    }
+    /*.circle-style:hover{*/
+    /*    transition: .5s;*/
+    /*    height: 300px;*/
+    /*    width: 300px;*/
+    /*}*/
 </style>
